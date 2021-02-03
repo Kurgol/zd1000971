b3bd3b6f4d50ef7e3faba93ed072bd5a7eceffd8
David Morris <gm3dmo@github.com>
2021-02-03 15:52:36 +0000

    Create Helloworld.cs

---
+++ HellowWorld/Helloworld.cs
@@ -0,0 +1,14 @@
+Copy
+using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+#if DebugConfig
+        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
+#endif
+
+        Console.WriteLine("Hello, world!");
+    }
+}
